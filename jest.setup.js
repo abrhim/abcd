4ad8820a35f97b969a4e513504dc91365289c4d1
Abram Himmer <abramhimmer@gmail.com>
2021-04-14 11:25:17 -0500

    mimicking package shape

--- jest.setup.js
+++
@@ -1,3 +0,0 @@
-import '@adobe/adobe-client-data-layer/dist/adobe-client-data-layer.min.js';
-
-window.adobeDataLayer = window.adobeDataLayer || [];
--- test/hooks/usePageUnload.test.js
+++
@@ -1,9 +0,0 @@
-import { renderHook } from '@testing-library/react-hooks';
-import usePageUnload from '../../lib/collector/hooks/usePageUnload';
-
-test('updates pageOffsets context and sends pageSummary event', () => {
-  const onBeforeUnload = jest.fn();
-  renderHook(() => usePageUnload(onBeforeUnload));
-  window.dispatchEvent(new CustomEvent('beforeunload'));
-  expect(onBeforeUnload).toHaveBeenCalledTimes(1);
-});
